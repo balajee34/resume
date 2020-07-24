8802b50cafd4da6460d0e4dad76590abf265fdb5
“Kumar <kumar.balajee@freshworks.com>
2020-07-25 02:19:15 +0530

    Updates

---
+++ precache-manifest.55e72c0399bf935ec02fdd6b2254bfa9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e47831a6b3686a0420c1c5351abae93",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "8aa4871fb2bc40dbb0df",
+    "url": "/resume/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "cb39e70ba88ea336ae33",
+    "url": "/resume/static/css/main.4d3929a0.chunk.css"
+  },
+  {
+    "revision": "8aa4871fb2bc40dbb0df",
+    "url": "/resume/static/js/2.deb2393e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/resume/static/js/2.deb2393e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb39e70ba88ea336ae33",
+    "url": "/resume/static/js/main.d8db3cb4.chunk.js"
+  },
+  {
+    "revision": "8c1d914a1677cea53979",
+    "url": "/resume/static/js/runtime-main.75d8c91e.js"
+  },
+  {
+    "revision": "5f826d26fb218c33be9043fb65c525d2",
+    "url": "/resume/static/media/Rochester-Regular.5f826d26.otf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/resume/static/media/logo.5d5d9eef.svg"
+  }
+]);
